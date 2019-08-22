617b1a7ea0be961d06e24f17c8ce5483fd94159d
Patrick Harrison <jahman@bellatlantic.net>
2019-08-22 17:48:40 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,6 +9,7 @@ attr_accessor :backers,:title
    end
 
    def add_backer(backer)
-   @Backer
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
    end
 end   
